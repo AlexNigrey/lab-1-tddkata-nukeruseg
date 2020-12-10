d56c040d1895f41036f2e44eae83a3006e62ddb0
nigrei.av <Nigrei.AV@omsk.gazprom-neft.ru>
2020-12-10 14:50:07 +0600

    refactoring

--- TDDKata/StringCalc.cs
+++ TDDKata/StringCalc.cs
@@ -10,11 +10,24 @@ namespace TDDKata
         private const int ERROR = -1;
         internal int Sum(string input)
         {
-            string[] delimeters = new string[] { ",", "\n" };
             if (input == null)
                 return ERROR;
             if (input.Equals(string.Empty))
                 return 0;
+
+            var arguments = SplitString(input);
+            foreach (string argument in arguments)
+            {
+                if (!int.TryParse(argument, out int intArgument) || intArgument < 0)
+                    return ERROR;
+            }
+            var intArguments = arguments.Select(a => int.Parse(a));
+            return intArguments.Sum();
+        }
+
+        private string[] SplitString(string input)
+        {
+            string[] delimeters = new string[] { ",", "\n" };
             if (input.StartsWith("//") && input.Contains("\n"))
             {
                 int newLineIdx = input.IndexOf("\n");
@@ -23,14 +36,8 @@ namespace TDDKata
                 delimeters = new string[] { delimeter };
                 input = input.Substring(newLineIdx + 1, input.Length - newLineIdx - 1);
             }
-            string[] arguments = input.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
-            foreach(string argument in arguments)
-            {
-                if (!int.TryParse(argument, out int intArgument) || intArgument < 0)
-                    return ERROR;
-            }
-            var intArguments = arguments.Select(a => int.Parse(a));
-            return intArguments.Sum();
+
+            return input.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
